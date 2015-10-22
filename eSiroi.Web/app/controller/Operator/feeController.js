017da64c7efd23b65d90c375b63105f1bfff8334
nj.singh@nic.in <Ntombi80>
2015-10-22 17:13:10 +0530

    fee_started

---
+++ eSiroi.Web/app/controller/Operator/feeController.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+//#region enterFeeController
+(function () {
+    angular.module('eSiroi.Web')
+    .controller('enterFeeController', ['$scope', '$state','deptModalService', enterFeeController]);
+    function enterFeeController($scope, $state, deptModalService) {
+
+    }
+})();
+//#endregion
